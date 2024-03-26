ff8bac5af9cc1c25717de539b3b6d7fcfd43ab4e
BApfa12 <164639566+BApfa12@users.noreply.github.com>
2024-03-26 12:46:55 +0100

    Update server.js

--- app/server.js
+++ app/server.js
@@ -41,7 +41,7 @@ let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
 let databaseName = "my-db";
 app.post('/update-profile', function (req, res) {
   let userObj = req.body;
-  MongoClient.connect(mongoUrl, mongoClientOptions, function (err, client) {
+  MongoClient.connect('mongodb://admin:password@mongodb:27017/my-db', mongoClientOptions, function (err, client) {
     if (err) throw err;
     let db = client.db(databaseName);
     userObj['userid'] = 1;
@@ -58,7 +58,7 @@ app.post('/update-profile', function (req, res) {
 app.get('/get-profile', function (req, res) {
   let response = {};
   // Connect to the db
-  MongoClient.connect(mongoUrl, mongoClientOptions, function (err, client) {
+  MongoClient.connect('mongodb://admin:password@mongodb:27017/my-db', mongoClientOptions, function (err, client) {
     if (err) throw err;
     let db = client.db(databaseName);
     let myquery = { userid: 1 };
